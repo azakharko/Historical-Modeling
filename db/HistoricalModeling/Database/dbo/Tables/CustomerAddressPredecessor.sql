39443cb5a1e9b7fe0b3372030e9041839e2ff3a3
BREEDLOVE\Andriy.Zakharko <andrew_root@ukr.net>
2017-11-09 13:19:32 -0600

    WIP

--- db/HistoricalModeling/Database/dbo/Tables/CustomerAddressPredecessor.sql
+++ db/HistoricalModeling/Database/dbo/Tables/CustomerAddressPredecessor.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[CustomerAddressPredecessor] (
     [CustomerAddressId]      INT NOT NULL,
-    [PriorCustomerAddressId] INT NOT NULL,
+    [PriorCustomerAddressId] INT NULL,
+    [DateModified] DATETIME2 NULL, 
     CONSTRAINT [FK_CustomerAddressPredecessor_CustomerAddress] FOREIGN KEY ([CustomerAddressId]) REFERENCES [dbo].[CustomerAddress] ([CustomerAddressId]),
     CONSTRAINT [FK_CustomerAddressPredecessor_CustomerAddress1] FOREIGN KEY ([PriorCustomerAddressId]) REFERENCES [dbo].[CustomerAddress] ([CustomerAddressId])
 );
